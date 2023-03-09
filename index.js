44ee2857cf3bd5fd7179669f75676728b18cc9d5
natalika-voz <kochyrovanatalia@gmail.com>
2023-03-09 23:30:36 +0500

    Add swiper

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('Hello, World!');
+import Swiper, { Navigation } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+const swiper = new Swiper('.swiper', {
+  modules: [Navigation],
+  navigation: {
+    nextEl: ".reviews__next",
+    prevEl: ".reviews__prev",
+    disabledClass: 'disabled',
+  },
+  spaceBetween: 20,
+  direction: "vertical",
+});
